dc8efb98137b1c5f5f79e288e84b21f071435191
Terence Lee <hone02@gmail.com>
2021-03-29 19:17:56 -0500

    redirect to the buildpack invite url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use gotham::{
+    helpers::http::response::create_permanent_redirect,
+    hyper::{Body, Response},
+    state::State,
+};
+
+/// Redirect to slack invite
+fn slack_invite(state: State) -> (State, Response<Body>) {
+    let resp = create_permanent_redirect(
+        &state,
+        "https://join.slack.com/t/buildpacks/shared_invite/zt-odkxxq0z-EqYmUbATpDLcX1c8nZCglQ",
+    );
+
+    (state, resp)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port = std::env::var("PORT").unwrap_or("3000".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+    gotham::start(addr, || Ok(slack_invite))
 }
